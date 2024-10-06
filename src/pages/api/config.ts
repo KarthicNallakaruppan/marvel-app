41c7b9bf83d859dd017507b2045b2a63fc7f401a
KarthicNallakaruppan <77838482+KarthicNallakaruppan@users.noreply.github.com>
2024-10-07 10:26:01 +1300

    Updated marvel API key

--- src/pages/api/config.ts
+++ src/pages/api/config.ts
@@ -1,3 +1,3 @@
 // config.ts
 export const MARVEL_API_BASE_URL = 'https://gateway.marvel.com/v1/public';
-export const MARVEL_API_KEY = '791352a55ad823175b8dd529934a0cde';
+export const MARVEL_API_KEY = '8602014864f461e673a85ca142599b53';
